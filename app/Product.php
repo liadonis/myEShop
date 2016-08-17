8413b9da49baba618e0e40715577bf3e4aeba82e
liadonis <li.adonis@gmail.com>
2016-08-17 10:33:09 +0800

    use \App\Product as P;

--- app/Product.php
+++ app/Product.php
@@ -6,14 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends BaseModel
 {
-    protected $table = "products"; //更改對應的資料表名稱
+//    protected $table = "products"; //更改對應的資料表名稱
 
-//    protected $table = "categories"; //更改對應的資料表名稱
-    protected $primaryKey = "id"; //設定所對應的主鑑
+
+//    protected $primaryKey = "id"; //設定所對應的主鑑
 
 //    protected $timestamps = false; //取消系統預設的寫入新增及更新時間
 
-    protected $fillable = ["pro_name","pro_title"]; //若routes使用create方式寫入資料要在這裡加上這一行,有寫的才會寫入
+//    protected $fillable = ["pro_name","pro_title"]; //若routes使用create方式寫入資料要在這裡加上這一行,有寫的才會寫入
 
+
+//    //=============測試寫入其他table==============
+//    protected $table = "categories"; //更改對應的資料表名稱
 //    protected $fillable = ["cate_name"];
+//    //==========================================
 }
